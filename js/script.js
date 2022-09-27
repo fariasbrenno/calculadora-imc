4ac340b529f7c979c2e70dd04f416784c3c0bac1
Brenno Farias <brenno.developer@gmail.com>
2022-09-27 13:14:13 -0300

    Avançando nos cálculos js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@ let altura = document.querySelector("#input-altura");
 let peso = document.querySelector("#input-peso");
 
 /*Funções*/
-function msg(){
-    alert("Mensagem");
+function calculo(a, b){
+    return (a*a)/b;
 }
 
 /*Eventos*/
+document.querySelector("#btn-calcular").addEventListener("click", function(){
+    let divResultado = document.getElementById("resultado");
+    divResultado.style.display = "block";
+
+    let inNome = nome.value;
+    let inAltura = altura.value;
+    let inPeso = peso.value;
+
+    console.log(calculo(inAltura, inPeso));
+    
+})
